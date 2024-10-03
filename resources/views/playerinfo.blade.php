344e4d1214502fe9bf8a997a66605d27bc827e29
Slice <51338223+SliceCraft@users.noreply.github.com>
2024-10-04 00:39:08 +0200

    More player info

--- resources/views/playerinfo.blade.php
+++ resources/views/playerinfo.blade.php
@@ -1,5 +1,15 @@
 <x-mainlayout>
-    <h1>{{$player->username}} {{$player->user_id}}</h1>
+    <a href="https://osu.ppy.sh/users/{{$player->user_id}}" style="color: white;">
+        <h1 class="mb-5">{{$player->username}}</h1>
+    </a>
+    <ul class="list-group mb-5 transparent-list-group">
+        <li class="list-group-item" data-bs-theme="dark">Completed daily challenges: {{$player->completed_daily_challenges}}</li>
+        <li class="list-group-item">Average accuracy: {{$player->average_accuracy}}</li>
+        <li class="list-group-item">Total attempts: {{$player->total_attempts}}</li>
+        <li class="list-group-item">Current streak: {{$player->current_streak}}</li>
+        <li class="list-group-item">Total score: {{$player->total_score}}</li>
+        <li class="list-group-item">Average placement: {{$player->average_placement}}</li>
+    </ul>
     <table class="table text-white table-bordered shadow p-3 mb-5 rounded">
         <thead>
         <tr>
