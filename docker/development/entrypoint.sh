fb0dc374bff24e54add77d6fe37d4fe06819ab2c
SliceCraft <51338223+SliceCraft@users.noreply.github.com>
2026-04-25 00:46:18 +0200

    Fix entrypoint bugs

--- docker/development/entrypoint.sh
+++ docker/development/entrypoint.sh
@@ -3,12 +3,13 @@
 HOST_UID=$(stat -c "%u" /var/www)
 HOST_GID=$(stat -c "%g" /var/www)
 
-if [ "$UID" != "0" ]; then
+if [ "$HOST_UID" != "0" ]; then
     echo "Updating permissions"
     usermod -u $HOST_UID www-data
     groupmod -g $HOST_GID www-data
 fi
 
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
 exec "$@"
