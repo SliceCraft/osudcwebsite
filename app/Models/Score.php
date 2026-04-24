8b52ccb8cc280274ccbf92eed21e96917e4fba9d
SliceCraft <51338223+SliceCraft@users.noreply.github.com>
2026-04-25 00:48:06 +0200

    Add dailyChallenge relationship

--- app/Models/Score.php
+++ app/Models/Score.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Score extends Model
 {
-
+    public function dailyChallenge(): HasOne
+    {
+        return $this->hasOne(DailyChallenge::class, 'id', 'daily_challenge');
+    }
 }
