995e8837ecbcb0b912ed4b47fb31bd0681688030
SliceCraft <51338223+SliceCraft@users.noreply.github.com>
2026-04-25 00:48:50 +0200

    Calculate total and weighted pp

--- app/Jobs/RecalculatePlayerData.php
+++ app/Jobs/RecalculatePlayerData.php
@@ -7,6 +7,7 @@ use App\Models\Player;
 use App\Models\Score;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Collection;
 
 class RecalculatePlayerData implements ShouldQueue
 {
@@ -26,7 +27,7 @@ class RecalculatePlayerData implements ShouldQueue
     public function handle(): void
     {
         $players = Player::query()->whereIn('user_id', $this->playerIds)->get();
-        $scores = Score::query()->whereIn('user_id', $this->playerIds)->get();
+        $scores = Score::query()->whereIn('user_id', $this->playerIds)->with(['dailyChallenge'])->get();
         $dailyChallenges = Score::max('daily_challenge');
 
         foreach($players as $player) {
@@ -61,8 +62,26 @@ class RecalculatePlayerData implements ShouldQueue
             // TODO: The totalplacement should use the bottom placement when a player didn't play in a particular daily challenge
             $player->average_placement = $totalPlacement / count($playerScores);
 
+            $playerScoresOnMainRuleset = $playerScores->filter(function ($row) {
+                return $row->dailyChallenge->ruleset_id == 0;
+            });
+            $player->total_pp = $playerScoresOnMainRuleset->pluck('pp')->sum();
+            $player->weighted_pp = $this->calculateWeightedPP($playerScoresOnMainRuleset);
+
             $player->save();
         }
+    }
+
+    function calculateWeightedPP(Collection $scores) : float
+    {
+        $scoresAsArray = $scores->sortByDesc(['pp'])->values()->toArray();
+        $total = 0;
+
+        foreach ($scoresAsArray as $rank => $score) {
+            $total += $score['pp'] * pow(0.95, $rank);
+        }
+
+        return $total;
     }
 
     public $timeout = 10000;
